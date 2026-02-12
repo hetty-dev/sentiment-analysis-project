67256112ffdc32cc43fd69600805e10e35a66652
Henrietta <ama1f@yahoo.com>
2026-02-12 09:46:18 +0100

    feat: Add command-line argument parsing

--- src/train.py
+++ src/train.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import argparse
 from sklearn.model_selection import train_test_split
 
 def load_and_validate_data(data_path: str) -> pd.DataFrame:
@@ -29,5 +30,9 @@ def split_data(
     return X_train, X_test, y_train, y_test
 
 if __name__ == "__main__":
-    df = load_and_validate_data("sentiments.csv")
-    print(df.head())
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data", default="data/sentiments.csv")
+    parser.add_argument("--out", default="models/sentiment.joblib")
+
+    args: argparse.Namespace = parser.parse_args()
+    main(data_path=args.data, model_path=args.out)
